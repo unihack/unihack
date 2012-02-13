2224260de8f8e9c4ef98e00577e2479c4cc1f0b7
Jens Ljungblad <jens.ljungblad@gmail.com>
2012-02-13 08:26:28 +0100

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unihack::Application.config.secret_token = '4e3f08e468072d40ffb9e4f625ae8600124e3d654cb763e3c3aeb1fae24863258b6010d605442e9bd9f35b9907061d768dd7aff591b932ff5ca80a07777854ea'
