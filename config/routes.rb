63545ca5ab96efb7a3c13c9ade63a933a0eadd22
Jens Ljungblad <jens.ljungblad@gmail.com>
2012-02-18 14:00:50 +0100

    rails tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Unihack::Application.routes.draw do
+
+  resources :teams do
+    resources :members
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Unihack::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
