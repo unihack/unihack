00d27288ecaf6e6869ab40eac10c372c1745b6e9
Jens Ljungblad <jens.ljungblad@gmail.com>
2012-04-18 23:50:49 +0200

    Close registration

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,26 +5,26 @@ class TeamsController < ApplicationController
   end
 
   # GET /teams/new
-  def new
-    @team = Team.new
-    4.times { @team.members.build }
-  end
+  # def new
+  #   @team = Team.new
+  #   4.times { @team.members.build }
+  # end
 
   # POST /teams
-  def create
-    @team = Team.new(params[:team])
-    members_copy = Array.new(@team.members)
-    @team.members.delete_if { 
-      |m| m.name.blank? && m.email.blank? && m.github.blank? 
-    }
+  # def create
+  #   @team = Team.new(params[:team])
+  #   members_copy = Array.new(@team.members)
+  #   @team.members.delete_if { 
+  #     |m| m.name.blank? && m.email.blank? && m.github.blank? 
+  #   }
     
-    respond_to do |format|
-      if @team.save
-        format.html { redirect_to teams_path }
-      else
-        @team.members = members_copy
-        format.html { render action: "new" }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @team.save
+  #       format.html { redirect_to teams_path }
+  #     else
+  #       @team.members = members_copy
+  #       format.html { render action: "new" }
+  #     end
+  #   end
+  # end
 end
